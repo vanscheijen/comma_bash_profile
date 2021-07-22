6e14f4dadde1a2cdd13a2d8843900bd95eec06a5
vanScheijen <github@vanscheijen.nl>
2021-07-22 10:00:14 +0200

    Added help and requirement check

--- function/,nicetimestamp.sh
+++ function/,nicetimestamp.sh
@@ -1,4 +1,9 @@
 ,nicetimestamp () {
+    local f_usage="[file]"
+    local f_info="Converts any unix timestamp in all lines given via stdin or in <file>"
+
+    ,,require perl || return
+
     /bin/cat "${1:--}" | /usr/bin/perl -pe 's/(\d+)/localtime($1)/e'
 }
 
