c5a85ca2924bb8b8715774a2ba327a7ccb08c0a5
vanScheijen <github@vanscheijen.nl>
2022-01-13 12:13:05 +0100

    Added ,gematria function, https://en.wikipedia.org/wiki/Gematria

---
+++ function/,gematria.sh
@@ -0,0 +1,20 @@
+,gematria () {
+    local f_usage="<word>"
+    local f_info="Show the sum of all individual letters of <word> where a=1,b=2,...,z=26"
+
+    local word
+    local -i i value
+
+    local -r spacelike="][ \.,;+=\t/\\(){}'\"\`"
+
+    tr "$spacelike" '\n' <<< "$@" |
+        sed 's/[^a-zA-Z]//g' |
+            ,lowercase | while read word; do
+        value=0
+        for ((i=0; i < ${#word}; i++)); do
+            value+=`printf "%d-96" "'${word:i:1}"`
+        done
+        echo "$word = $value"
+    done
+}
+
