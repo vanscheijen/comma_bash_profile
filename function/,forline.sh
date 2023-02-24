c7dafe2472fd2acf7787875680fddeee5189501e
vanScheijen <github@vanscheijen.nl>
2023-02-24 17:01:02 +0100

    Added parallel option to ,forline

--- function/,forline.sh
+++ function/,forline.sh
@@ -1,6 +1,12 @@
 ,forline () {
-    local f_usage="[file] [command]"
-    local f_info="Loops through each line(as \$line) in [file] or stdin (default) and executes [command] (defaults to echo \\\$line)"
+    local f_usage="[-p] [file] [command]"
+    local f_info="Loops through each line(as \$line) in [file] or stdin (default) and executes [command] (defaults to echo \\\$line). Optionally in parallel(-p)"
+
+    local parallel=0
+    if [[ "$1" == "-p" ]]; then
+        parallel=1
+        shift
+    fi
 
     local content
     if [[ -s "$1" ]]; then
@@ -16,7 +22,11 @@
 
     local line
     while IFS= read -r line; do
-        eval "$@"
+        if [[ "$parallel" == 1 ]]; then
+            eval "$@" &
+        else
+            eval "$@"
+        fi
     done <<< "$content"
 }
 
