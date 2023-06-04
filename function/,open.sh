1e781f6d21bbf24ce46123d7db7272ec78958b74
vanScheijen <github@vanscheijen.nl>
2023-06-04 22:24:33 +0200

    ,open url by default with https if protocol is not given

--- function/,open.sh
+++ function/,open.sh
@@ -1,12 +1,14 @@
 ,open () {
-    local f_usage="<file>"
-    local f_info="Opens <file> with the default application assigned via mime type"
+    local f_usage="<file | url>"
+    local f_info="Opens <file | url> with the default application assigned via mime type"
 
     local filetype app
     if [[ -f "$@" ]]; then
         filetype="$(xdg-mime query filetype "$@")"
         app="$(xdg-mime query default "$filetype")"
         echo "$filetype is opened by $app"
+    else
+        [[ "$@" =~ \. ]] && set "https://$@"
     fi
     xdg-open "$@"
 }
