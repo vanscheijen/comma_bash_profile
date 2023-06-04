805107f1fd59256950cd0593b4edca044d4d8bc8
vanScheijen <github@vanscheijen.nl>
2023-06-04 23:28:16 +0200

    Implemented auto-directory-creation in ,extract

--- function/,extract.sh
+++ function/,extract.sh
@@ -4,20 +4,38 @@
 
     [[ -s "$1" ]] || { ,,usage; return; }
 
+    local tool=""
     case $1 in
         *.tar.gz|*.tar.bz2|*.tar.xz|*.tar.lzip|*.tar.lzma|*.tar.lzop|*.tar.Z|*.tar.zstd|*.tbz2|*.tgz|*.tar)
-                tar xvafkp "$1" ;;
-        *.rar)  unrar x "$1" ;;
-        *.zip)  unzip "$1" ;;
-        *.7z)   7z x "$1" ;;
-        *.gz)   gunzip "$1" ;;
-        *.bz2)  bunzip2 "$1" ;;
-        *.Z)    uncompress "$1" ;;
-        *)      ,error "'$1' is an unknown compression format" ;;
+                tool="tar xvafkp" ;;
+        *.rar)  tool="unrar x" ;;
+        *.zip)  tool="unzip" ;;
+        *.7z)   tool="7z x" ;;
+        *.gz)   tool="gunzip" ;;
+        *.bz2)  tool="bunzip2" ;;
+        *.Z)    tool="uncompress" ;;
+        *)
+            case $(file -b --mime-type -- "$1") in
+                application/zip) tool="unzip" ;;
+            esac
     esac
 
+    [[ "$tool" ]] || { ,error "'$1' has an unknown compression format"; return 62; }
+    ,info "Using $tool to extract '$1'"
+    local origdir="$PWD"
+    local tmpdir
+    tmpdir="$(mktemp -d -p "$PWD")"
+    cd "$tmpdir" && $tool "$origdir/$1"
+    cd "$origdir"
+    if [[ $(\ls -d1q "$tmpdir/" | wc -l) -eq 1 && $(\ls -1q "$tmpdir/" | wc -l) -eq 1 ]]; then
+        mv -i "$tmpdir/"* "$origdir/"
+        rmdir "$tmpdir"
+    else
+        local newname="${1%%.*}"
+        [[ -e "$newname" ]] && newname="${newname}-extract"
+        mv "$tmpdir" "$newname"
+    fi
+
     # TODO: protect against zipbomb (how?)
-    # TODO: use mimetype instead of suffix?
-    # TODO: always ensure extraction goes inside a directory, and move back if only 1 dir is extracted
 }
 
