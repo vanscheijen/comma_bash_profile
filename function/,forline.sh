a6a32455d7a41551c8da5ae5366998756b592a30
vanScheijen <github@vanscheijen.nl>
2023-06-04 19:41:58 +0200

    Improved ,forline, e.g.: cat formulas.txt | ,forline ,c

--- function/,forline.sh
+++ function/,forline.sh
@@ -1,5 +1,5 @@
 ,forline () {
-    local f_usage="[-p] [file] <command>"
+    local f_usage="[-p] [file | stdin] <command>"
     local f_info="Loops through each line(as \$line) in [file] or stdin (default) and executes <command> (defaults to echo \\\$line). Optionally in parallel with [-p]"
 
     local parallel=0
@@ -8,17 +8,11 @@
         shift
     fi
 
-    local content
-    if [[ -s "$1" ]]; then
-        content="$(< "$1")"
-        shift
-    else
-        content="$(,ifne cat)"
-    fi
+    local data="$1"
+    [[ -s "$data" ]] && { data="$(< "$data")"; shift; } || data="$(,ifne cat)" || { ,,usage; return; }
 
-    [[ "$content" ]] || { ,,usage; return; }
-
-    [[ -n "$@" ]] || set "echo \"\$line\""
+    [[ -n "$@" ]] || set 'echo "$line"'
+    [[ "$@" =~ \$line ]] || set "$@" '"$line"'
 
     local line
     while IFS= read -r line; do
@@ -27,6 +21,6 @@
         else
             eval "$@"
         fi
-    done <<< "$content"
+    done <<< "$data"
 }
 
