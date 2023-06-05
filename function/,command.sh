d39d5ffe117e3e3c454f0583deb70df157253535
vanScheijen <github@vanscheijen.nl>
2023-06-05 19:17:41 +0200

    Fixed ,command so it doesn't break current shell

--- function/,command.sh
+++ function/,command.sh
@@ -2,23 +2,21 @@
     local f_usage="<command> [arguments...]"
     local f_info="Treat the <command> with optional <arguments...> as a simple command, suppressing any shell function lookups, wrong paths, etc."
 
-    # Stolen from man page of 'command'
+    # Run inside a subshell
+    (
+        # Set IFS to its default value of <space><tab><newline>.
+        IFS=' '$'\t'$'\n'
 
-    IFS=' '$'\t'$'\n'
-    #    The preceding value should be <space><tab><newline>.
-    #    Set IFS to its default value.
+        # Unset all possible aliases including unalias itself.
+        \unalias -a
 
-    \unalias -a
-    #    Unset all possible aliases.
-    #    Note that unalias is escaped to prevent an alias
-    #    being used for unalias.
+        # Ensure command is not a user function.
+        \unset -f command
 
-    unset -f command
-    #    Ensure command is not a user function.
+        # Put on a reliable PATH prefix.
+        PATH="$(command -p getconf PATH):$PATH"
 
-    PATH="$(command -p getconf PATH):$PATH"
-    #    Put on a reliable PATH prefix.
-
-    command "$@"
+        command "$@"
+    )
 }
 
