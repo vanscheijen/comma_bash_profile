b2d64f7b651fc8c3a978d481a48877f29120b4be
vanScheijen <github@vanscheijen.nl>
2023-06-07 22:16:36 +0200

    Added ,compress to easily pack an archive of files

---
+++ function/,compress.sh
@@ -0,0 +1,33 @@
+,compress () {
+    local f_usage="<archive> <file ...>"
+    local f_info="Automatically compress <file ...> into <archive> (7z|xz|bz2|gz|zip)"
+
+    [[ "$1" ]] || { ,,usage; return; }
+
+    local method
+    [[ "$1" =~ \.xz$ ]] && method=xz
+    [[ "$1" =~ \.t?bz2$ ]] && method=bz
+    [[ "$1" =~ \.t?gz$ ]] && method=gz
+    [[ "$1" =~ \.zip$ ]] && method=zip
+
+    if [[ -f "$1.7z" ]]; then
+        mv -f -- "$1.7z" "$1.7z.backup"
+    elif [[ -f "$1" ]]; then
+        mv -f -- "$1" "$1.backup"
+    fi
+
+    if [[ -z $method ]] && ,,have 7z; then
+        7z a -r -snh -snl -stl -y -- "$@"
+    elif [[ $method == xz ]] || [[ -z $method ]] && ,,have xz; then
+        tar -cJvpaf "$@"
+    elif [[ $method == bz ]] || [[ -z $method ]] && ,,have bzip2; then
+        tar -cjvpaf "$@"
+    elif [[ $method == gz ]] || [[ -z $method ]] && ,,have gzip; then
+        tar -czvpaf "$@"
+    elif [[ $method == zip ]] || [[ -z $method ]] && ,,have zip; then
+        zip -r9y "$@"
+    else
+        ,error "No compression available"
+    fi
+}
+
