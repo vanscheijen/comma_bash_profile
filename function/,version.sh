8bc578013917a98177e661a9b749640813e75544
vanScheijen <github@vanscheijen.nl>
2024-12-03 14:19:39 +0100

    Moved version info to ,version

---
+++ function/,version.sh
@@ -0,0 +1,7 @@
+,version () {
+    local f_info="Show Comma .bash_profile version"
+
+    # NOTE: Complementary colors FFF100 and 6C0BC9
+    ,echo "$(RGBCOLOR FF5600)Comma $(RGBCOLOR 00C786).bash_profile${RESET} ${DIMGRAY}v${DIMCYAN}${COMMA_VERSION}${DIMGRAY}-${DIMGREEN}${COMMA_DATE_GENERATED}${RESET} ©2022-2024 Fair License"
+}
+
--- profile.d/80_login.sh
+++ profile.d/80_login.sh
@@ -1,8 +1,7 @@
 # Login prompt
 
 if shopt -q login_shell; then
-    # NOTE: Complementary colors FFF100 and 6C0BC9
-    ,echo "$(RGBCOLOR FF5600)Comma $(RGBCOLOR 00C786).bash_profile${RESET} ${DIMGRAY}v${DIMCYAN}${COMMA_VERSION}${DIMGRAY}-${DIMGREEN}${COMMA_DATE_GENERATED}${RESET} ©2022 Fair License"
+    ,version
     ,box "$(uname -a)"
     w
 fi
